67549673d58e09651c0b260c6be402ed57fb846e
pwartenb <77082390+pwartenb@users.noreply.github.com>
2022-01-07 09:26:34 -0500

    Reload of files on 1/7

---
+++ Color_block_recogn.py
@@ -0,0 +1,224 @@
+import cv2
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+from Cam_dev import *
+from pydexarm import Dexarm
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+
+switch1 = 5
+switch2 = 6
+# switch3 = 20
+# switch4 = 21
+
+GPIO.setup(switch1, GPIO.IN)
+GPIO.setup(switch2, GPIO.IN)
+# GPIO.setup(switch3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.setup(switch4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+'''windows'''
+#dexarm = Dexarm(port="COM67")
+'''mac & linux'''
+dexarm = Dexarm(port="/dev/ttyACM0")
+
+tar_color = 'green'
+color_dict = {'red': {'Lower': np.array([127, 60, 171]), 'Upper': np.array([188, 197, 255])},
+              'blue': {'Lower': np.array([100, 80, 46]), 'Upper': np.array([124, 255, 255])},
+              'green': {'Lower': np.array([35, 43, 35]), 'Upper': np.array([90, 255, 255])},
+              }              
+
+red_hsv = [108, 190, 120, 255, 163, 223]
+blue_hsv = [74, 131, 107, 241, 146, 255]
+yellow_hsv = [30, 83, 60, 209, 156, 255]
+
+feature_param=[40,40,60,250,20]
+
+rgb_param=[0,0,255]
+
+
+class Color_block_recogn():
+    tar_num = 0
+
+    tar_info = {"num":0,
+            "center":[],
+            "angle":[]}
+    
+
+    def __init__(self,color_list=[],fea_list=[],rect_rgb_list=[]):
+        self.hsv = color_list
+        self.fea_p = fea_list
+        self.rect_rgb = rect_rgb_list
+        print("Color hsv"+str(self.hsv))
+        print("Target characteristics"+str(self.fea_p))
+        print("Rectangular border"+str(self.rect_rgb))
+        pass
+    
+    def set_hsv(self,tar_list=[]):
+        self.hsv = tar_list
+        pass
+
+    def set_fea(self,tar_list=[]):
+        self.fea_p = tar_list
+        pass
+
+    def set_rect_rgb(self,tar_list=[]):
+        self.rect_rgb = tar_list
+        pass
+
+    def get_target_img(self,img,condition_index):
+        
+
+        temp_tar_info = {"num":0,
+        "center":[],
+        "angle":[]}
+        temp_num = 0
+
+
+        gs_img = cv2.GaussianBlur(img, (5, 5), 0)                     
+
+        hsv_img = cv2.cvtColor(gs_img, cv2.COLOR_BGR2HSV)
+
+        inRange_hsv = cv2.inRange(hsv_img, np.array([self.hsv[0],self.hsv[2],self.hsv[4]]), np.array([self.hsv[1],self.hsv[3],self.hsv[5]]))
+
+        average_val_img = cv2.blur(inRange_hsv,(3,3))
+
+        canny_img = cv2.Canny(average_val_img,128,255,3)
+
+        _,contours, hierarchy = cv2.findContours(canny_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        try:
+            for i in range(len(contours)):
+                # if (len(contours[i])>self.fea_p[2]) and (len(contours[i])<self.fea_p[3]):
+                # if (len(contours[i])>self.fea_p[2]):
+                if (len(contours[i])>35):
+                
+                    min_rect = cv2.minAreaRect(contours[i])
+
+  
+                    if condition_index == 1:
+
+                        result = (min_rect[1][0]>35 and min_rect[1][1]>35)and(abs(min_rect[1][0]-min_rect[1][1])<25)
+                    elif condition_index == 2:
+                        result = (min_rect[1][0] * min_rect[1][1]) > 670
+
+                    # if (min_rect[1][0]>35 and min_rect[1][1]>35)and(abs(min_rect[1][0]-min_rect[1][1])<25):
+                    # if ((min_rect[1][0] * min_rect[1][1]) > 670):
+                    if result:
+ 
+                        box_points = cv2.boxPoints(min_rect)
+
+                        cv2.circle(img,(int(min_rect[0][0]),int(min_rect[0][1])) ,2,(self.rect_rgb[0],self.rect_rgb[1], self.rect_rgb[2]),4)
+                        cv2.drawContours(img, [np.int0(box_points)], 0, (self.rect_rgb[0],self.rect_rgb[1], self.rect_rgb[2]), 2)        
+                        
+               
+                        temp_tar_info["center"].insert(temp_num,np.int0(min_rect[0]))
+                        temp_tar_info["angle"].insert(temp_num,np.int0(min_rect[2]))  
+
+                        temp_num = temp_num + 1
+
+            temp_tar_info["num"]=temp_num
+
+            self.tar_info.clear()
+            self.tar_info = temp_tar_info
+            # print(self.tar_info)
+        except:
+            print("error------------->")
+
+        return img,inRange_hsv
+
+
+    def get_tar_info(self):
+        pass
+    
+    pass
+
+
+def recogn_main():
+    video.open(0,320,240)
+    revogn = Color_block_recogn(red_hsv,feature_param,rgb_param)
+    # while True:
+    #     #img,inRange_hsv = revogn.get_target_img(video.get_img(1))
+    #     #cv2.imshow('inRange_hsv', inRange_hsv)
+    #     #cv2.imshow('src_img', img)
+    #     #cv2.waitKey(1)
+    #     break
+    return video.get_img(0)
+
+def trim_image(x, y, im):
+    list_im = list(im)
+    x_start = 75
+    x_end = 200
+    y_start = 140
+    y_end = 180
+    new_im = list_im[x_start: x_end+1]
+    final_im = []
+    for item in new_im:
+        final_im.append(item[y_start: y_end+1])
+    return np.array(final_im)
+
+def get_av_pixel(im):
+    total_num = len(im)*len(im[0])
+    total = 0
+    for row in im:
+        for item in row:
+            av = sum(item)/3
+            total += av
+    return total/total_num
+
+def ford_46(dexarm):
+    current = (0, 340, 150)
+    dexarm.fast_move_to(41, 309, 30)
+    dexarm.fast_move_to(-20, 320, 30)
+    dexarm.fast_move_to(-20, 330, 30)
+    dexarm.fast_move_to(-20, 340, 30)
+    dexarm.fast_move_to(-20, 350, 30)
+    dexarm.fast_move_to(-20, 360, 30)
+    dexarm.fast_move_to(41, 371, 30)
+    dexarm.fast_move_to(0, 340, 150)
+
+if __name__ == "__main__":
+    print("Ready to go")
+    dexarm.fast_move_to(0, 340, 150)
+    video.open(0,320,240)
+    previous = "Black"
+    start = 0
+    dexarm.conveyor_belt_forward(8300)
+    while True:
+        img = video.get_img(0)[:,:,::-1]
+        img = trim_image(320, 240, img)
+        av_pixel = get_av_pixel(img)
+        if av_pixel < 185:
+            if previous == "White":
+                end = time.perf_counter()
+                elapsed = end - start
+                dist = elapsed*71.5
+                print("Len of panel: ", (dist/25.4), " inches")
+                length = dist/25.4
+                if 5.5 < length < 6.5:
+                    dexarm.conveyor_belt_backward(8300)
+                    time.sleep(elapsed*.8)
+                    dexarm.conveyor_belt_stop()
+                    ford_46(dexarm)
+                    dexarm.dealy_s(2)
+                dexarm.conveyor_belt_forward(8300)
+                previous = "Black"
+                img = video.get_img(0)[:,:,::-1]
+                img = trim_image(320, 240, img)
+                av_pixel = get_av_pixel(img)
+                while av_pixel > 180:
+                    img = video.get_img(0)[:,:,::-1]
+                    img = trim_image(320, 240, img)
+                    av_pixel = get_av_pixel(img)
+        else:
+            if previous == "Black":
+                previous = "White"
+                start = time.perf_counter()
+        if av_pixel < 35:
+            dexarm.conveyor_belt_stop()
+            break
+    pass
---
+++ README.md
@@ -0,0 +1,3 @@
+# BASF_robotics
+
+Files used for autonomous testing of film thickness using Rotrics robotic arm and conveyor belt.
