a5c1322065f485482ef05eed57b9c78427f4c7cc
Patrick Whartenby <pwartenb@mit.edu>
2022-01-12 13:45:09 -0500

    update

--- G_code_convert.py
+++ G_code_convert.py
@@ -1,7 +1,9 @@
+import serial
 import re
 
 class CNC:
-    """ Python class for CNC Machine
+    """ 
+    Python class for CNC Machine
     """
 
     def __init__(self, port):
@@ -12,19 +14,19 @@ class CNC:
         self.ser = serial.Serial(port, 115200, timeout=None)
         self.is_open = self.ser.isOpen()
         if self.is_open:
-            print('pydexarm: %s open' % self.ser.name)
+            print('CNC: %s open' % self.ser.name)
         else:
             print('failed to open serial port')
 
     def _send_cmd(self, data, wait=True):
         """
-        Send command to the arm.
+        Send command to the CNC machine.
 
         Args:
             data (string): the command
-            wait (bool): wait for response from the arm (ok) or not.
+            wait (bool): wait for response from the machine (ok) or not.
                 If True, this function will block until the arm response "ok"
-                If False, this function will not block here. But the command could be ignored if buffer of the arm is full.
+                If False, this function will not block here. But the command could be ignored if buffer is full.
         """
         self.ser.write(data.encode())
         if not wait:
@@ -42,7 +44,7 @@ class CNC:
             except:
                 print('no val')
 
-    def move_to(self, x=None, y=None, z=None, e=None, feedrate=2000, mode="G1", wait=True):
+    def move_to(self, x=None, y=None, z=None, e=None, feedrate=2000, mode="G01", wait=True):
         """
         Move to a cartesian position. This will add a linear move to the queue to be performed after all previous moves are completed.
 
@@ -88,7 +90,7 @@ class CNC:
                     if serial_str.find("ok") > -1:
                         return x, y, z
 
-cnc = CNC(port="/dev/ttyACM0")
+cnc = CNC(port="/dev/ttyprintk")
 
 if __name__ == "__main__":
     with open('CNC_G_Code/test.txt') as f:
@@ -105,13 +107,11 @@ if __name__ == "__main__":
     while True:
         pos = cnc.get_current_position()
         print(pos)
-        x_pos = int(pos[0])
-        y_pos = int(pos[1])
         z_pos = int(pos[2])
         delta_z = float(input("Change Z by how much?"))
         if delta_z == 0:
             cnc._send_cmd("M30")
             cnc._send_cmd("%")
             break
-        cnc.move_to(x_pos, y_pos, z_pos + delta_z)
+        cnc.move_to(z = z_pos + delta_z)
 
